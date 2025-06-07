5eaf73157f5da62303c2b74b468e24abe9d213fc
Lindenbyte <kimlindbla@gmail.com>
2025-06-07 13:19:58 +0200

    Random cpp file

---
+++ test/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Probably not valid, but for testing what ever!
+template<typename T>
+void print_something(char* msg, T test) {
+	std::cout << msg << " " << test << std::endl;
+}
+
+int main(int argc, const char** argv) {
+	print_something("Hello, World!", 0x1350)
+	
+	int x;
+	x = 1337;
+
+	float d = 0.1351f;
+	double f = 0.001036103613061;
+	char* well = "hello, there!";
+
+	return 0;
+}
